3195c112e4ef4b9fd675e8c1f5201f6269e1209f
Timothy Maurer <timothygmaurer@outlook.com>
2025-03-13 08:55:26 +0100

    Update mongo_db.py and main.py

--- db/mongo_db.py
+++ db/mongo_db.py
@@ -1,18 +1,50 @@
 from pymongo import MongoClient
-
-
+import datetime
 
 # Connect to MongoDB (default: localhost, port 27017)
 client = MongoClient("mongodb://localhost:27017/")
 
 # Create a database (it will be created when you insert a document)
-db = client["my_database"]
+db = client["MongoNotes"]
 
-# Create a collection
-collection = db["my_collection"]
+# Create a collections
+notes = db["Notes"] # has Tags
+users = db["Users"]
 
-# Insert a sample document
-sample_data = {"name": "Alice", "age": 25, "city": "New York"}
-collection.insert_one(sample_data)
+## Repository
 
-print("Database initialized successfully!")
+# notes:
+def add_note(title, content, weight, status, tags, owner_id):
+    note = {"title": title, 
+            "content": content, 
+            "created_at": datetime.datetime.now(),
+            "updated_at": datetime.datetime.now(),
+            "weight": weight,
+            "status": status,
+            "tags": tags, # list of tags
+            "owner_id": owner_id
+            }
+    notes.insert_one(note)
+
+def get_all_notes():
+    return notes.find()
+
+def get_notes_by_title(title):
+    return notes.find({"title": title})
+
+def get_notse_by_id(id):
+    return notes.find({"_id": id})
+
+def update_note_by_id(id, title, content, weight, status, tags):
+    notes.update_one({"_id": id}, 
+                     {"$set": {"title": title, 
+                               "content": content, 
+                               "updated_at": datetime.datetime.now(),
+                               "weight": weight,
+                               "status": status,
+                               "tags": tags
+                               }
+                     })
+    
+def delete_note_by_id(id):
+    notes.delete_one({"_id": id})
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from db import mongo_db
 
+#init connection
+mongo_db.connent()
 
+is_running = True
+
+while (is_running):
+    # Todo
